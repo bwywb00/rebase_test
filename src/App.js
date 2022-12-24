641836aea92dbf8576352f26109aea068ebe73f5
YOON <yoon@YOONui-MacBookPro.local>
2022-12-24 14:35:29 +0900

    [yoon] login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+<<<<<<< HEAD
 import Main from "./Main/Main";
+=======
+import Login from "./Login/Login";
+>>>>>>> a30afd7 ([yoon] login)
 
 const App = () => {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Main />
+=======
+      <Login />
+>>>>>>> a30afd7 ([yoon] login)
     </div>
   );
 };
